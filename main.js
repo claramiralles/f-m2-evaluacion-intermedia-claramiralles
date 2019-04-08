eefdc2b0bfed456a13bc7ff47d965bdae562b421
Clara Miralles de Imperial Polo <clara@Claras-MacBook-Air.local>
2019-04-08 18:14:23 +0200

    function handler Access To Input And Show It In Console done

--- main.js
+++ main.js
@@ -4,4 +4,16 @@ function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
   }
   console.log('> ' + getRandomNumber(100));
-  
+
+  //al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
+
+  let buttonEl=document.getElementById('player__button');
+  let inputEl=document.getElementById('player__input');
+
+  function handlerAccessToInputAndShowItInConsole(event){
+      buttonEl=event.currentTarget;
+      console.log(inputEl.value);
+  }
+  buttonEl.addEventListener('click', handlerAccessToInputAndShowItInConsole);
+
+
