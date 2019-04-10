2cbf5bed953258e6655807590eb9ac22316625f2
Clara Miralles <clara.miralles.imperial@gmail.com>
2019-04-10 08:16:03 +0200

    improves functions in js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,36 +1,45 @@
 'use strict';
 
+let buttonEl=document.getElementById('player__button');
+const inputEl=document.getElementById('player__input');
+const randomNumber=getRandomNumber(100);
+const guessNumber=parseInt (inputEl.value); 
+
+
+const counterText=document.querySelector('.counter');
+const counterEl= counter +=1;
+const counter=counterText.innerHTML;
+//arreglar el counter que esto no tiene lógica
+
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
-  }
-const randomNumber= getRandomNumber(100);
+}
+function handlerAccessToInputAndPaintIt(event){
+    buttonEl = event.currentTarget;
+    console.log(inputEl.value);
+}
+buttonEl.addEventListener('click', handlerAccessToInputAndPaintIt); 
+//al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
 
-  //al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
+function compareInputWithRandomAndPaintIt () {
+if (randomNumber < guessNumber){
+    console.log('Demasiado alto');
+}
+else if (guessNumber < randomNumber) {
+    console.log ('Demasiado bajo');
+}
+else if (guessNumber === randomNumber) {
+    console.log ('¡HAS GANADO, CAMPEONA!');
+}
+const userTries=compareInputWithRandomAndPaintIt ();
 
-  const buttonEl=document.getElementById('player__button');
-  const inputEl=document.getElementById('player__input');
+//Aqui estoy perdida. Mirar ejemplos de funciones e intentar solucionarlo. Si no ver ejercicio de Nasi a ver que me falta.
+}
 
-  function handlerAccessToInputAndShowItInConsole(event){
-      buttonEl=event.currentTarget;
-      console.log(inputEl.value);
-  }
-  buttonEl.addEventListener('click', handlerAccessToInputAndShowItInConsole);
+
 
 
 //comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el
 //feedback correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS
 //GANADO, CAMPEONA!")
 
-function compareInputNumberWithRandomNumberAndPaintItInConsole () {
-if ( randomNumber < parseInt(inputEl.value)){
-    console.log('Demasiado alto');
-}
-else if (parseInt (inputEl.value) < randomNumber) {
-    console.log ('Demasiado bajo');
-}
-else if (parseInst (inputEl.value) === randomNumber) {
-    console.log ('¡HAS GANADO, CAMPEONA!');
-}
-} 
-
-compareInputNumberWithRandomNumberAndPaintItInConsole ();
